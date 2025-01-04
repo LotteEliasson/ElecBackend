57656845ec54cf51c6dff61041f660444c1129f5
LotteEliasson <lottefacereliasson@hotmail.com>
2025-01-04 16:41:51 +0100

    dynamic port listenerr

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.get('/', (req, res) =>{
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('server is running on port 5000');
+  console.log('server is running on port ${PORT}');
   
 })
